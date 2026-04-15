8495321ab51be9a27156547647be5d03bae6c89f
branjam <>
2026-04-15 21:23:49 +0000

    Add view library
    FossilOrigin-Name: d6bd60dde164e3bfe971cfa86583b56915bbf1906af74be3ea36f9c299ee9dc1

---
+++ ebmm-view.el
@@ -0,0 +1,60 @@
+;;; ebmm-view.el --- Enterprise Business Motivation Model views in Emacs -*- lexical-binding: t -*-
+
+;; Author: Brandon Ellington
+;; Version: 0.0.1
+;; Package-Requires: eieio-base
+;; Homepage: nil
+;; Keywords: convenience,enterprise
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Work with Enterprise Business Motivation Model objects and views on
+;; those objects.  This library sets up the views from the metamodel.
+
+
+;;; Code:
+(require 'ebmm-elements)
+
+;;;; Variables
+(defvar ebmm-view-plist
+  (let-alist ebmm-raw-alist
+    (seq-keep
+     (pcase-lambda (`(_ ,(map xmi:id) .
+			,(map ('elements `(_ _ . ,elements))
+			      properties project)))
+       (if elements
+	   (let-alist (car properties)
+	     (append
+	      (list :name .name)
+	      (let-alist (car project)
+		(list :eaid xmi:id
+		      :created .created
+		      :modified .modified))
+	      (list :documentation .documentation)
+	      (list :elements
+		    (seq-mapcat (pcase-lambda (`(element ,(map subject)))
+				  (seq-keep (pcase-lambda ((map :eaid :name))
+					      (if (string= eaid subject)
+						  name))
+					    ebmm-elements))
+				elements))))))
+     .xmi:XMI.xmi:Extension.diagrams))
+  "Viewpoints from the Enterprise Business Motivation Model.")
+
+(provide 'ebmm-view)
+;;; ebmm-view.el ends here
